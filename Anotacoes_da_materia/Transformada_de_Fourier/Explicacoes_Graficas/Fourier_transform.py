ebd742f8bc04fb0a549a32a24f9176b48a90ea94
Ricardo <131534712+RicardoV1e1r4@users.noreply.github.com>
2026-06-06 17:34:57 -0300

    Alteração na função atualizar
    Um gráfico stem() é composto por:
    
    StemContainer
    ├── markerline   → bolinhas
    ├── stemlines    → hastes verticais
    └── baseline     → linha horizontal
    
    Ao mover o slider:
    
    '''stem.markerline.set_ydata(y)'''
    
    atualiza as bolinhas.
    
    E:
    
    '''stem.stemlines.set_segments(segmentos)'''
    
    atualiza as hastes verticais.
    
    Assim o gráfico é redesenhado sem remover e recriar objetos, o que é muito mais rápido quando o número de amostras é grande.

--- Anotacoes_da_materia/Transformada_de_Fourier/Explicacoes_Graficas/Fourier_transform.py
+++ Anotacoes_da_materia/Transformada_de_Fourier/Explicacoes_Graficas/Fourier_transform.py
@@ -16,14 +16,14 @@ Ts = 1/fs
 t_final = 60
 t = np.arange(0, t_final, Ts)
 
-omega = 0.1
+omega = 0.6
 
 
 # Taxa de amostragem
 def geracao_sinal(w, const=False):
     if const==True:
         # Sinal x[n]
-        y_n = np.cos(t)
+        y_n = np.cos(0.5*t)
     else:
         # Sinal variável h[h]
         y_n = np.sin(w*t)
@@ -32,8 +32,12 @@ def geracao_sinal(w, const=False):
 
 # Criação dos gráficos
 figure, (eix1, eix2) = plt.subplots(2, 1, figsize=(8,7))
+
 # Espaço extra para os botões deslizantes
-plt.subplots_adjust(left=0.12, bottom=0.15, hspace=0.5)
+plt.subplots_adjust(
+    left=0.12,
+    bottom=0.3,
+    hspace=0.5)
 
 # configuração do gráfico do sinal x[n] original
 eix1.stem(t, geracao_sinal(omega, True))
@@ -61,33 +65,30 @@ eix2.grid(True)
 # slider_sample = Slider(fsample, 'Amostragem', 0.01, 5, valinit=fs)
 
 # #Deslizante 2 - Frequência angular
-freq_ang = figure.add_axes([0.2, 0.25, 0.65, 0.03])
+freq_ang = figure.add_axes([0.15, 0.03, 0.7, 0.03])
 
 slider_omega = Slider(
     ax=freq_ang,
-    label='Frequêncai angular',
+    label='Frequência angular',
     valmin=0.1,
-    valmax=30,
+    valmax=1,
     valinit=omega,)
 
 ###___Função que vai atualizar o gráfico___###
 def atualizar(val):
-    global stem2
-    omega = slider_omega.val
+    w = slider_omega.val
     
-    # Remover o gráfico anterior
-    stem2.makerline.remove()
+    y = geracao_sinal(w)
     
-    try:
-        stem2.stemlines.remove()
-    except:
-        for linha in stem2.stemlines:
-            linha.remove()
-            
-    stem2.baseline.remove()
+    # Atualiza os marcadores
+    stem2.markerline.set_ydata(y)
     
-    # Cria o novo gráfico stem
-    stem2 = eix2.stem(t, geracao_sinal(omega, False))
+    # Atualiza as hastes
+    segmentos = [
+        [(x, 0), (x, yi)]
+        for x, yi in zip(t, y)]
+    
+    stem2.stemlines.set_segments(segmentos)
     
     # raioa = slider_raioa.val
     # eix1.set_ydata(PreencheDensidade(raioa, raiob))
