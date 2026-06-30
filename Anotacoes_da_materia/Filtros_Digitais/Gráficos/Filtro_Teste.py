85ab407972eebf98da051b9a7d25f847a8146461
Ricardo <131534712+RicardoV1e1r4@users.noreply.github.com>
2026-06-29 22:30:35 -0300

    Melhoria nos gráficos
    Ajuste na plotagem dos gráficos em uma matriz 3x2.
    Mas ainda tem polir a plotagem geral

--- Anotacoes_da_materia/Filtros_Digitais/Gráficos/Filtro_Teste.py
+++ Anotacoes_da_materia/Filtros_Digitais/Gráficos/Filtro_Teste.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Jun  7 13:16:05 2026
+Created on Mon Jun 29 21:57:52 2026
 
 @author: Ricardo Alexandre
 """
@@ -12,24 +12,41 @@ from scipy.signal import freqz
 pi = np.pi
 
 # parâmetros dos polos
-r = 0.8
+r = 0.7
 
-w1 = 0.25
-w2 = 0.35
+# Frequências
+fs = 60000
 
-mb1 = -2*r*np.cos(w1)
-mb2 = r**2
+f1 = 5000
+f2 = 20000
 
-ma1 = -2*r*np.cos(w2)
-ma2 = r**2
+# rad/segundos
+Omega_1 = 2*pi*f1
+Omega_2 = 2*pi*f2
+
+# rad/amostras
+w1 = 2*pi*(f1/fs)
+w2 = 2*pi*(f2/fs)
+print(w1)
+print(w2)
+
+# Passa-baixas
+m_baixa1 = -2*r*np.cos(w2)
+m_baixa2 = r**2
+print(m_baixa1)
+
+# Passa-alta
+m_alta1 = -2*r*np.cos(w1)
+m_alta2 = r**2
+print(m_alta1)
 
 # coeficientes do passa baixa
 num1 = [1, 2, 1]
-den1 = [1, mb1, mb2]
+den1 = [1, m_baixa1, m_baixa2]
 
 # coeficientes do passa-alta
 num2 = [1, -2, 1]
-den2 = [1, ma1, ma2]
+den2 = [1, m_alta1, m_alta2]
 
 # resposta em frequência passa-baixa
 w1, H1 = freqz(num1, den1, 2048)
@@ -49,7 +66,7 @@ eix1.plot(w1, np.abs(H1))
 # eix1.plot(w2, np.abs(H2))
 eix1.set_xlabel(r'$\omega$ (rad/amostras)')
 eix1.set_ylabel(r'$|H(e^{j\omega})|$')
-eix1.set_title('Filtro Rejeita-Faixa de 2ª Ordem')
+eix1.set_title('Filtro de 2ª Ordem')
 eix1.grid(True)
 
 # eix2 = plt.subplot(3, 2, 3)
@@ -68,23 +85,23 @@ eix3.grid(True)
 
 # eix4 = plt.subplot(3, 2, 2)
 eix4 = figure.add_subplot(spec[0, 1])
-eix4.plot(w1/(2*pi), np.abs(H1))
+eix4.plot(fs*w1/(2*pi), np.abs(H1))
 # eix4.plot(w2, np.abs(H2))
 eix4.set_xlabel(r'$\omega/2\pi$')
 eix4.set_ylabel(r'$|H(e^{j\omega})|$')
-eix4.set_title('Filtro Rejeita-Faixa de 2ª Ordem')
+eix4.set_title('Filtro de 2ª Ordem')
 eix4.grid(True)
 
 # eix5 = plt.subplot(3, 2, 4)
 eix5 = figure.add_subplot(spec[1, 1])
-eix5.plot(w2/(2*pi), np.abs(H2), color='r')
+eix5.plot(fs*w2/(2*pi), np.abs(H2), color='r')
 eix5.set_xlabel(r'$\omega/2\pi$')
 eix5.set_ylabel(r'$|H(e^{j\omega})|$')
 eix5.grid(True)
 
 # eix6 = plt.subplot(3, 2, 6)
 eix6 = figure.add_subplot(spec[2, 1])
-eix6.plot(w1/(2*pi), np.abs(H3))
+eix6.plot(fs*w1/(2*pi), np.abs(H3))
 eix6.set_xlabel(r'$\omega/2\pi$')
 eix6.set_ylabel(r'$|H(e^{j\omega})|$')
 eix6.grid(True)
