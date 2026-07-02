b7a1376d37b24d398a870eddb8a48a94cf971e59
Ricardo <131534712+RicardoV1e1r4@users.noreply.github.com>
2026-07-02 16:25:15 -0300

    Ajustes no código
    - Adição do vetor de pesos w[]
    - Ajuste no laço for que faz o produto escalar do vetor de pesos e o vetor steering
    - E ajuste na lógica do cálculo do ganho

--- Trabalho_de_Processamento_de_Sinais/Array_Vector.py
+++ Trabalho_de_Processamento_de_Sinais/Array_Vector.py
@@ -10,14 +10,15 @@ import numpy as np
 
 # Variáveis
 pi = np.pi
-exp = np.e
 
 fs = 1000
 ts = 1/fs
 
-d = 0.5
-lambd = 0.1
+fator = 1.5
+lambd = 0.8
+d = lambd*fator
 
+# d = 0.8
 # Vetores
 qtd_antenas = 2
 antenas = np.arange(0, qtd_antenas, 1)
@@ -27,6 +28,9 @@ angles = np.arange(-pi/2, pi/2, ts)
 
 #
 psi = 2*pi*(d/lambd)*np.sin(angles)
+
+# Vetor de pesos
+w = np.ones(qtd_antenas)
 
 # Calculo do Steering Vector
 steering_vector = np.zeros((len(angles), qtd_antenas), dtype=complex)
@@ -41,14 +45,13 @@ for indice, k in enumerate(psi):
 AF = np.zeros(len(angles), dtype=complex)
 
 for indice, m in enumerate(AF):
-    AF[indice] = sum(steering_vector[indice, :])
+    # AF[indice] = sum(steering_vector[indice, :])
+    AF[indice] = np.conj(w) @ steering_vector[indice]
 
 # Cálculo do Ganho
-AF_db = np.zeros(len(angles))
-
-for indice, m in enumerate(AF):
-    module = abs(m/2)
-    AF_db[indice] = 10*np.log10(module**2)
+AF_mod = abs(AF)
+AF_mod = AF_mod/np.max(AF_mod)
+AF_db = 20*np.log10(AF_mod)
     
-plt.plot(angles, AF_db)
+plt.plot(180*angles/pi, AF_db)
 plt.show()
