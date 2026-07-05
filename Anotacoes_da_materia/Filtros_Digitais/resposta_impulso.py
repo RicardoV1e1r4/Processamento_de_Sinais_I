1c393aa39826e23a60a8b70b38fb6255df90383c
Ricardo <131534712+RicardoV1e1r4@users.noreply.github.com>
2026-07-05 13:07:16 -0300

    Programa resposta ao Impulso
    Programa que uni ao funções de filtros com as funções Janela

---
+++ Anotacoes_da_materia/Filtros_Digitais/resposta_impulso.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  4 22:14:09 2026
+
+@author: Ricardo Alexandre
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import freqz
+import Filtros
+import Funções_Janela as fj
+
+pi = np.pi
+M = 80
+
+n = np.arange(0, M + 1, 1)
+
+Omega_c1 = 2000
+Omega_c2 = 4000
+Omega_s  = 10000
+
+wc1 = (Omega_c1*2*pi)/Omega_s
+wc2 = (Omega_c2*2*pi)/Omega_s
+
+pb = Filtros.passa_baixas(M, wc1)
+
+pa = Filtros.passa_altas(M, wc2)
+
+rejeita_faixa = pb + pa
+
+window_r = fj.janela_retangular(M)
+window_h = fj.janela_hamming(M)
+
+hn_linha = rejeita_faixa * window_h
+
+omega1, Hhamm = freqz(hn_linha, 1, 2048)
+
+# plt.stem(n, pa)
+plt.plot(Omega_s * omega1/(2 * pi), 20 * np.log10(abs(Hhamm)/max(abs(Hhamm))))
+# plt.ylim((-200, 1))
+
+plt.show()
