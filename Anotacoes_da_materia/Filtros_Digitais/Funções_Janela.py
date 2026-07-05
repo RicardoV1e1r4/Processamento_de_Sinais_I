122cb9218e27df7a780a0a836f577acdb77d2006
Ricardo <131534712+RicardoV1e1r4@users.noreply.github.com>
2026-07-04 22:57:26 -0300

    Criação de programa
    Criação do programa com as funções janela

--- Anotacoes_da_materia/Filtros_Digitais/Funções_Janela.py
+++ Anotacoes_da_materia/Filtros_Digitais/Funções_Janela.py
@@ -11,39 +11,30 @@ import numpy as np
 # Variáveis
 pi = np.pi
 
-M  = 80
-wc1 = pi/4
-wc2 = pi/2
+# Janela Retangular
+def janela_retanguar(M):
+    wr = np.ones(2(int(M//2)) + 1)
+    return wr
 
-Omega_c1 = 2000  #rad/s
-Omega_c2 = 4000  #rad/s
-Omega_s  = 10000 #rad/s
+# Janela Triangular
+def janela_triangular(M):
+    n_pos = np.arange(1, int(M/2)+1)
+    
+    wt0 = 1
+    wt_pos = -(2*n_pos)/(M + 2) + 1
+    
+    wt = np.concatenate((np.flip(wt_pos), [wt0], wt_pos))
+    
+    return wt
 
-wc1 = 2*pi*(Omega_c1/Omega_s)
-wc2 = 2*pi*(Omega_c2/Omega_s)
-
-# Função h[n]
-n = np.arange(0, M//2 + 1, 1)
-print("Comprimento", len(n))
-
-h0 = 1 - (wc2 - wc1)/pi
-
-hn = np.where(n == 0, h0, (np.sin(wc1*n) - np.sin(wc2*n))/(pi*n))
-
-# Janela de Hamming wH[n]
-alpha = 0.54
-wn = alpha + (1 - alpha)*np.cos((2*pi*n)/M)
-
-# Valores arredondados
-hn_round = np.round(hn, decimals=6)
-
-wn_round = np.round(wn, decimals=6)
-
-# Multiplicação h[n] com wH[n]
-hn_linha = hn_round * wn_round
-
-# print(n)
-# print(h0)
-# print("h_d[n]:", hn_round)
-# print("wn:", wn_round)
-# print("h_linha[n]:", hn_linha)
+# Janelas de Hamming e de Hann
+def janela_hamming(M):
+    n_pos = np.arange(1, int(M//2) + 1)
+    
+    alpha = 0.54
+    wh0 = alpha + (1 - alpha)*np.cos((2*pi*0)/M)
+    wh_pos = alpha + (1 - alpha)*np.cos((2*pi*n_pos)/M)
+    
+    wh = np.concatenate(np.flip(wh_pos), [wh0], wh_pos)
+    
+    return wh
