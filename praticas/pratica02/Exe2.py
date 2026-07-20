7e7877456dc324799c3e55d547a7e2c01f9894b1
Ricardo <131534712+RicardoV1e1r4@users.noreply.github.com>
2026-07-20 19:53:02 -0300

    Create Exe2.py
    Resolução do exercício 2 da aula prática 2

---
+++ praticas/pratica02/Exe2.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 20 18:44:48 2026
+
+@author: Ricardo Alexandre
+"""
+
+#%%
+'''
+2. Para um chirp com frequência inicial f0 = 500 Hz, frequência final f1 = 10000 Hz,
+amostrado com fs = 44.1 kHz, faça o que se pede nos itens abaixo:
+    
+(a) Utilizando a função calculate_spectrum(), calcule o espectro do chirp com varreduras
+de frequência lineares, quadráticas e logarítmicas.
+
+(b) Comente os resultados obtidos.
+
+'''
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.fft import fft, fftfreq
+
+#%%
+# Parâmetros
+pi = np.pi
+
+f0 = 500
+f1 = 5e3
+
+fs = 44.1e3
+
+duracao = 0.01
+
+#%%
+def calculate_spectrum(signal, sampling_frequency, single_sided=True):
+    """
+    Calculates the amplitude spectrum of a signal.
+
+    Args:
+        signal (np.ndarray): The time-domain signal.
+        sampling_frequency (float): The sampling frequency of the signal (Hz).
+        single_sided (bool): If True, returns the single-sided spectrum (positive frequencies).
+                             If False, returns the full spectrum (positive and negative frequencies).
+
+    Returns:
+        tuple: A tuple containing:
+            - frequencies (np.ndarray): Array of frequencies (Hz).
+            - amplitudes (np.ndarray): Array of corresponding amplitude magnitudes.
+    """
+    N = len(signal) # Number of sample points
+    T = 1.0 / sampling_frequency # Sample spacing
+
+    # Perform the FFT
+    yf = fft(signal)
+
+    if single_sided:
+        xf = fftfreq(N, T)[:N//2] # Frequencies for the positive half of the spectrum
+        # Calculate the single-sided amplitude spectrum
+        # np.abs(yf[0:N//2]) gets the magnitude of the positive frequencies
+        amplitudes = 1.0/N * np.abs(yf[0:N//2])
+    else:
+        xf = fftfreq(N, T) # All frequencies
+        amplitudes = 1.0/N * np.abs(yf)
+
+    return xf, amplitudes
+#%%
+time = np.arange(0, duracao, duracao/fs)
+
+#%%
+# Chirp Linear
+a1 = (f1 - f0)/duracao**2
+
+linear_chirp = np.cos(2*pi*(a1*time + f0)*time)
+
+plt.plot(time, linear_chirp)
+plt.xlabel("Time (s)")
+plt.ylabel("Amplitude")
+plt.xlim(-0.5*duracao, 1.5*duracao)
+plt.grid(True)
+
+plt.show()
